94960abe383db7f73aa87eb4af17b5c40a6c04e5
joshthompson <josh3276@gmail.com>
2026-05-03 11:53:47 +0000

    Deploying to gh-pages from @ joshthompson/scoopbusrunclub@bc343b49f40e46aa0f7c7e6e2600855188551b05 🚀

---
+++ game/assets/tgaTextureLoader-DN_Zell9.js
@@ -0,0 +1,1 @@
+import{dd as r,de as n}from"./LevelObjects-FXEIaYCH.js";import"./index-BKxs35eA.js";class f{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,a){const o=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),s=r(o);a(s.width,s.height,t.generateMipMaps,!1,()=>{n(t,o)})}}export{f as _TGATextureLoader};
